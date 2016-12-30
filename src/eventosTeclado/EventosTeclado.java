4cbf906fa8e20dce1cd39f5df8470e04fc32d04a
rparias <rparias@udlanet.ec>
2016-12-30 17:18:37 -0500

    prueba desde pc escritorio

--- src/eventosTeclado/EventosTeclado.java
+++ src/eventosTeclado/EventosTeclado.java
@@ -1,14 +1,20 @@
 package eventosTeclado;
 
+import javax.swing.*;
+
 /**
  * Creado por Ronald Arias - fecha: 30-dic-16.
  */
 public class EventosTeclado {
     public static void main(String[] args){
-
+        MarcoTeclado miMarco = new MarcoTeclado();
     }
 }
 
-class MarcoTeclado{
-
+class MarcoTeclado extends JFrame{
+    public MarcoTeclado(){
+        setVisible(true);
+        setTitle("Eventos de teclado");
+        setBounds(600, 300, 400, 400);
+    }
 }
