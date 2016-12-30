eb21b7ec3666fc13fd068852b67353b1fb80529e
rparias <rparias@udlanet.ec>
2016-12-30 17:38:09 -0500

    Eventos con teclado

--- src/eventosTeclado/EventosTeclado.java
+++ src/eventosTeclado/EventosTeclado.java
@@ -1,6 +1,7 @@
 package eventosTeclado;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 /**
  * Creado por Ronald Arias - fecha: 30-dic-16.
@@ -16,5 +17,25 @@ class MarcoTeclado extends JFrame{
         setVisible(true);
         setTitle("Eventos de teclado");
         setBounds(600, 300, 400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //instancia del oyente
+        EventoDeTeclado tecla = new EventoDeTeclado();
+        addKeyListener(tecla);
+
     }
+}
+
+//-----CLASE DEL OBJETO OYENTE-----//
+class EventoDeTeclado implements KeyListener{
+    //metodos de la interfaz
+    public void keyTyped(KeyEvent e) {
+        char letra = e.getKeyChar();
+        System.out.println("La tecla presionada es: " + letra);
+    }
+    public void keyPressed(KeyEvent e) {
+        int codigo = e.getKeyCode();
+        System.out.println("Codigo: " + codigo);
+    }
+    public void keyReleased(KeyEvent e) {}
 }
