4812d92cd9bed17a11690083c8e12dda2d8536a4
rparias <ron_1192@hotmail.com>
2016-12-30 16:36:05 -0500

    Eventos con teclado

---
+++ src/eventosTeclado/EventosTeclado.java
@@ -0,0 +1,14 @@
+package eventosTeclado;
+
+/**
+ * Creado por Ronald Arias - fecha: 30-dic-16.
+ */
+public class EventosTeclado {
+    public static void main(String[] args){
+
+    }
+}
+
+class MarcoTeclado{
+
+}
