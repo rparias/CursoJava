e3f48b5ba3b4441ec53235ef2c23e881e70aabad
rparias <ron_1192@hotmail.com>
2017-01-02 22:42:51 -0500

    Layouts 1

---
+++ src/layouts/Layouts_I.java
@@ -0,0 +1,38 @@
+package layouts;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Creado por Ronald Arias - fecha: 02-ene-17.
+ */
+public class Layouts_I {
+    public static void main(String[] args){
+        MarcoLayout miMarco = new MarcoLayout();
+    }
+}
+
+class MarcoLayout extends JFrame{
+    public MarcoLayout(){
+        setVisible(true);
+        setTitle("Layouts en Java");
+        setBounds(600, 300, 600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //instancias
+        PanelLayout miLamina = new PanelLayout();
+        FlowLayout disposicion = new FlowLayout(FlowLayout.LEFT);   //alinear a la izq
+        miLamina.setLayout(disposicion);
+        add(miLamina);
+    }
+}
+
+class PanelLayout extends JPanel{
+    public PanelLayout(){
+        add(new JButton("Amarillo"));
+        add(new JButton("Azul"));
+        add(new JButton("Rojo"));
+
+
+    }
+}
