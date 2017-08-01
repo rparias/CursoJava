706fa33f78d3d1a30068ca9e623e5c703f6f6d3b
rparias <ron_1192@hotmail.com>
2017-08-01 17:56:15 -0500

    Validar email con cuadros de texto

--- src/cuadrosTexto/PruebaTexto.java
+++ src/cuadrosTexto/PruebaTexto.java
@@ -1,6 +1,7 @@
 package cuadrosTexto;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -24,27 +25,57 @@ class MarcoTexto extends JFrame{
 
 class LaminaTexto extends JPanel{
     private JTextField txtCampo1;
+    private JLabel lblResultado;
 
+    //--->Lamina Principal con borderLayout
     public LaminaTexto() {
-        //label
-        JLabel lblEtiqueta = new JLabel("Email:");
-        add(lblEtiqueta);
+        //disposicion de elementos
+        setLayout(new BorderLayout());
 
-        //cuadro de texto
-        txtCampo1 = new JTextField(15);
-        JButton boton1 = new JButton("Aceptar");
-        add(txtCampo1);
+        //--->Sublamina con elementos en fila
+        JPanel subLamina = new JPanel();
+        subLamina.setLayout(new FlowLayout());
 
-        //boton
-        DameTexto miEvento = new DameTexto();
-        boton1.addActionListener(miEvento);
-        add(boton1);
+            //label campo
+            JLabel lblEtiqueta = new JLabel("Email:");
+            subLamina.add(lblEtiqueta);
+
+            //cuadro de texto
+            txtCampo1 = new JTextField(15);
+            JButton boton1 = new JButton("Aceptar");
+            subLamina.add(txtCampo1);
+
+            //label resultado
+            lblResultado = new JLabel("", JLabel.CENTER);
+            subLamina.add(lblResultado);
+
+            //boton
+            DameTexto miEvento = new DameTexto();
+            boton1.addActionListener(miEvento);
+            subLamina.add(boton1);
+
+        //agrego la lamina y la sublamina
+        add(subLamina, BorderLayout.NORTH);
+        add(lblResultado, BorderLayout.CENTER);
     }
 
     private class DameTexto implements ActionListener{
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            int correcto = 0;
+            String email = txtCampo1.getText().trim();
+
+            for(int i=0; i<email.length(); i++){
+                if(email.charAt(i)=='@')
+                    correcto++;
+            }
+
+            if(correcto!=1)
+                lblResultado.setText("Este email no es valido");
+            else
+                lblResultado.setText("Email Correcto!");
+
             System.out.println(txtCampo1.getText().trim()); //trim quita espacios antes de la cadena
         }
     }
