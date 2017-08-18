4871e715aad34c8588ec5206fc02103441e1b211
rparias <ron_1192@hotmail.com>
2017-08-17 19:25:45 -0500

    ArrayList I

--- src/programacion_generica/UsoEmpleado.java
+++ src/programacion_generica/UsoEmpleado.java
@@ -1,11 +1,28 @@
 package programacion_generica;
 
+import java.util.ArrayList;
+
 public class UsoEmpleado {
     public static void main(String[] args) {
-        Empleado listaEmpleados[] = new Empleado[3];
+        /*Empleado listaEmpleados[] = new Empleado[3];
         listaEmpleados[0] = new Empleado("Ronald", 25, 300);
         listaEmpleados[1] = new Empleado("Gabby", 24, 500);
-        listaEmpleados[2] = new Empleado("Erick", 18, 900);
+        listaEmpleados[2] = new Empleado("Erick", 18, 900);*/
+
+        ArrayList<Empleado> listaEmpleados = new ArrayList<Empleado>();
+
+        //para ahorrar recursos de memoria puedo definir el tamaño del arrayList si es q lo sé
+        //listaEmpleados.ensureCapacity(4);
+
+        listaEmpleados.add(new Empleado("Ronald", 25, 300));
+        listaEmpleados.add(new Empleado("Gabby", 24, 500));
+        listaEmpleados.add(new Empleado("Erick", 18, 900));
+        listaEmpleados.add(new Empleado("Majo", 21, 600));
+
+        //para ahorrar un exceso de memoria, se puede cerrar el arrayList si no se agregan mas elementos
+        listaEmpleados.trimToSize();
+
+        System.out.println("Elementos en total: " + listaEmpleados.size());
 
         for(Empleado e: listaEmpleados){
             System.out.println(e.getDatos());
